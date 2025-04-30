e4f0ff128ccd6683b16349afd437ad041be03876
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-30 14:57:46 -0300

    feat: Added the dashBoardActions to request for the backend, created a middleware to correctly render the dashboard component and changed the port of the backend.

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -11,7 +11,7 @@ server.use(bodyParser.json());
 server.use(cors);
 server.use(queryParser);
 //port config
-const port = 3006;
+const port = 2020;
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`);
 });
