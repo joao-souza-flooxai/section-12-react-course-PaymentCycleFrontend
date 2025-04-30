e1a1e08316cb9e1e59ac0002021884a42d997cd2
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-30 13:41:19 -0300

    feat: Added the app structure: header, menu, content and footer.

--- my-money-app/frontend/webpack.config.js
+++ my-money-app/frontend/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     ],
     module: {
         loaders: [{
-                test: /\.js[x]?$/,
+                test: /.js[x]?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
