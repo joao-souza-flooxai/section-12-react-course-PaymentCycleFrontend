3c4630d8ac0c877b7b96002b89e38bd3b2eb9281
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-05-02 12:27:52 -0300

    feat: Tab component expanded, connected to Redux and visibility according to application state.

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import DashboardReducer from '../dashboard/dashboardReducer';
+import TabReducer from '../common/tab/tabReducer';
 //Estado global da aplicação que será passada para os componentes pertinentes. 
 const rootReducer = combineReducers({
-    dashboard: DashboardReducer
+    dashboard: DashboardReducer,
+    tab: TabReducer
 });
 
 export default rootReducer;
